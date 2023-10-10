83a89aedb1784ea9fb65d9a5921509e68c3a8b37
muhammed-sayeed <sayeedmon25@gmail.com>
2023-10-10 15:32:28 +0530

    services and projects sections completed

--- assets/js/main.js
+++ assets/js/main.js
@@ -29,3 +29,24 @@ const linkAction = () =>{
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
+//===========SWIPER JS==================
+let swiperProjects = new Swiper(".projects__container", {
+    loop: true,
+    spaceBetween: 24,
+
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+    breakpoints: {
+        1200: {
+          slidesPerView: 2,
+          spaceBetween: -56,
+        },
+      },
+    mousewheel: true,
+    keyboard: true,
+  });
