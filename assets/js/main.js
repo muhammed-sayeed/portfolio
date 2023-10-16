7b5ad984195d3fcc61c1187d710317ff0446b2ce
muhammed-sayeed <sayeedmon25@gmail.com>
2023-10-16 07:06:54 +0530

    project section updated to category based listing

--- assets/js/main.js
+++ assets/js/main.js
@@ -44,7 +44,8 @@ let swiperProjects = new Swiper(".projects__container", {
     breakpoints: {
         1200: {
           slidesPerView: 2,
-          spaceBetween: -56 ,
+           spaceBetween: 25 ,
+           
         },
       },
     
@@ -149,7 +150,83 @@ const sr = ScrollReveal({
   duration: 2500,
   delay: 400,
 })
-sr.reveal(`.home__data, .projects__container, .footer__container`)
+sr.reveal(`.home__data, .project-section, .footer__container, .services__container`)
 sr.reveal(`.home__info div`,{delay: 600, origin: 'bottom', interval: 100})
 sr.reveal(`.skills__content:nth-child(1), contact__content:nth-child(1)`,{origin: 'left'})
 sr.reveal(`.skills__content:nth-child(2), contact__content:nth-child(2)`,{origin: 'right'})
+
+function revealProject(){
+  sr.reveal('.project-card',{
+    interval:150,
+    origin:'bottom'
+  })
+}
+
+
+
+//-------------=====================----------------
+const projects = [
+  {
+    name: 'Project 1',
+    category: 'js',
+  },
+  {
+    name: 'Project 2',
+    category: 'ds',
+  },
+  {
+    name: 'Project 3',
+    category: 'js',
+  },
+  {
+    name: 'Project 4',
+    category: 'js',
+  },
+  {
+    name: 'Project 5',
+    category: 'js',
+  },
+  {
+    name: 'Project 6',
+    category: 'js',
+  }
+  // Add more projects with categories
+];
+
+revealProject()
+
+// Function to display projects based on the selected category
+function displayProjects(category) {
+  const projectContainer = document.querySelector('.project-container');
+  projectContainer.innerHTML = ''; // Clear the container
+
+  projects.forEach((project) => {
+    if (category === 'all' || project.category === category) {
+      const projectElement = document.createElement('div');
+      projectElement.classList.add('project-card');
+      projectElement.textContent = project.name;
+      projectContainer.appendChild(projectElement);
+    }
+  });
+  revealProject()
+}
+
+// Event listeners for filter buttons
+const jsFilterButton = document.getElementById('js-filter');
+const dsFilterButton = document.getElementById('ds-filter');
+const allFilterButton = document.getElementById('all-filter');
+
+jsFilterButton.addEventListener('click', () => {
+  displayProjects('js');
+});
+
+dsFilterButton.addEventListener('click', () => {
+  displayProjects('ds');
+});
+
+allFilterButton.addEventListener('click', () => {
+  displayProjects('all');
+});
+
+// Initial display (all projects)
+displayProjects('all');
